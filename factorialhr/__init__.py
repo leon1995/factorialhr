ae9489612c5fd1623e92ac258b8aa31271496507
Leon <y6q6ea9w@mail-proxy.org>
2023-03-23 10:03:11 +0100

    fix pipeline

--- factorialhr/__init__.py
+++ factorialhr/__init__.py
@@ -1,10 +1,11 @@
 import os
 
 try:
-    __version__ = os.environ['FACTORIALHR_VERSION']
+    __version__ = os.environ["FACTORIALHR_VERSION"]
 except KeyError:
     from importlib import metadata
+
     try:
-        __version__ = metadata.version('factorialhr')
+        __version__ = metadata.version("factorialhr")
     except metadata.PackageNotFoundError:
-        __version__ = '0.0.0'
+        __version__ = "0.0.0"
